74c897f62cf237f53297e1510cf07222b0da4680
Indirakumar <88279768+Indhar01@users.noreply.github.com>
2022-08-30 15:50:36 +0530

    Update profile1.php

--- Php/profile1.php
+++ Php/profile1.php
@@ -117,21 +117,30 @@
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js"  crossorigin="anonymous"></script>
         <script src="../Js/profile.js" type="text/javascript"></script>
+	<script type="text/javascript">
+		$("#update").submit(function(){
+	</script>
         <?php
-      if(isset($_POST['cno']) && isset($_POST['address'])  && isset($_POST['uname'])){
-        $cno = $_POST['cno'];
-        $address = $_POST['address'];
-        
-      $query = "UPDATE user_details1 SET  cno= '$cno',address = '$address' WHERE uname = '$uname'";
+      		if(isset($_POST['cno']) && isset($_POST['address'])  && isset($_POST['uname'])){
+		$cno = $_POST['cno'];
+		$address = $_POST['address'];
+
+	      $query = "UPDATE user_details1 SET  cno= '$cno',address = '$address' WHERE uname = '$uname'";
                     $result = mysqli_query($conn, $query);
-                    ?>
+		if(result){
+         ?>
+	
         <script type="text/javascript">
             alert("Update Successfull.");
             window.location = "index.html";
         </script>
+	
         <?php
+		}
              }               
-?>
-    
+	?>
+    <script>
+	     });
+</script>
 </body>
 </html>
