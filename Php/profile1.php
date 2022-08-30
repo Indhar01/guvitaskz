083b78fd177e1eb526e55f5db779c2430264ee14
Indirakumar <88279768+Indhar01@users.noreply.github.com>
2022-08-30 15:53:04 +0530

    Update profile1.php

--- Php/profile1.php
+++ Php/profile1.php
@@ -117,9 +117,7 @@
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js"  crossorigin="anonymous"></script>
         <script src="../Js/profile.js" type="text/javascript"></script>
-	<script type="text/javascript">
-		$("#update").submit(function(){
-	</script>
+	
         <?php
       		if(isset($_POST['cno']) && isset($_POST['address'])  && isset($_POST['uname'])){
 		$cno = $_POST['cno'];
@@ -139,8 +137,6 @@
 		}
              }               
 	?>
-    <script>
-	     });
-</script>
+   
 </body>
 </html>
